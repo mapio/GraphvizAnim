850d0b3d02ead30dc8be10b1434a04805310325f
Massimo Santini <santini@di.unimi.it>
2016-01-31 22:39:56 +0100

    Done a couple of todos

--- gvanim/__main__.py
+++ gvanim/__main__.py
@@ -15,31 +15,43 @@
 # You should have received a copy of the GNU General Public License along with
 # "GraphvizAnim". If not, see <http://www.gnu.org/licenses/>.
 
-from sys import stdin, argv
+from argparse import ArgumentParser, FileType
+from sys import stdin, argv, stderr
 
 import animation, render
 
+def main():
 
-if len( argv ) != 2:
-	dest = 'dest'
-else:
-	dest = argv[ 1 ]
+	parser = ArgumentParser( prog = 'gvanim' )
+	parser.add_argument( 'animation', nargs = '?', type = FileType( 'r' ), default = stdin, help = 'The file containing animation commands (default: stdin)' )
+	parser.add_argument( '--delay', '-d', default = '100', help = 'The delay (in ticks per second, default: 100)' )
+	parser.add_argument( 'basename', help = 'The basename of the generated file' )
+	args = parser.parse_args()
 
-ga = animation.Animation()
+	ga = animation.Animation()
+	cmd2method = {
+		'ns' : ga.next_step,
+		'an' : ga.add_node,
+		'hn' : ga.highlight_node,
+		'rn' : ga.remove_node,
+		'ae' : ga.add_edge,
+		'he' : ga.highlight_edge,
+		're' : ga.remove_edge,
+	}
 
-cmd2method = {
-	'ns' : ga.next_step,
-	'an' : ga.add_node,
-	'hn' : ga.highlight_node,
-	'rn' : ga.remove_node,
-	'ae' : ga.add_edge,
-	'he' : ga.highlight_edge,
-	're' : ga.remove_edge,
-}
+	for line in args.animation:
+		parts = line.strip().split()
+		cmd, params = parts[ 0 ], parts[ 1: ]
+		try:
+			cmd2method[ cmd ]( *params )
+		except KeyError:
+			print >>stderr, 'gvanim: unrecognized command: {}'.format( cmd )
+			return
+		except TypeError:
+			print >>stderr, 'gvanim: wrong number of parameters: {}'.format( line.strip() )
+			return
 
-for line in stdin:
-	parts = line.strip().split()
-	cmd, params = parts[ 0 ], parts[ 1: ]
-	cmd2method[ cmd ]( *params )
+	render.to_gif( render.render( ga.to_graphs(), args.basename, 'png' ), args.basename, args.delay )
 
-render.to_gif( render.render( ga.to_graphs(), dest, 'png' ), dest, 50 )
+if __name__ == '__main__':
+	main()
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'GraphvizAnim',
+	version = '0.1-alpha',
+	description = 'A tool to create animated graph visualizations, based on graphviz',
+	author = 'Massimo Santini',
+	author_email = 'santini@di.unimi.it',
+	url = 'https://github.com/mapio/GraphvizAnim',
+	packages = [ 'gvanim' ],
+)
