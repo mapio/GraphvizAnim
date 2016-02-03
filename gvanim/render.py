c606bee0652ec5356f0bf93ae954857504036e50
Massimo Santini <santini@di.unimi.it>
2016-02-03 20:43:58 +0100

    Now render uses multiprocessing

--- gvanim/render.py
+++ gvanim/render.py
@@ -16,16 +16,21 @@
 # "GraphvizAnim". If not, see <http://www.gnu.org/licenses/>.
 
 from subprocess import Popen, PIPE, STDOUT, call
+from multiprocessing import Pool, cpu_count
+
+def _render( params ):
+	path, fmt, size, graph = params
+	with open( path , 'w' ) as out:
+		pipe = Popen( [ 'dot',  '-Gsize=1,1!', '-Gdpi={}'.format( size ), '-T', fmt ], stdout = out, stdin = PIPE, stderr = None )
+		pipe.communicate( input = graph )
+	return path
 
 def render( graphs, basename, fmt = 'png', size = 320 ):
-	paths = []
-	for n, graph in enumerate( graphs ):
-		path = '{}_{:03}.{}'.format( basename, n, fmt )
-		with open( path , 'w' ) as out:
- 			pipe = Popen( [ 'dot',  '-Gsize=1,1!', '-Gdpi={}'.format( size ), '-T', fmt ], stdout = out, stdin = PIPE, stderr = None )
-			pipe.communicate( input = graph )
-		paths.append( path )
-	return paths
+	try:
+		_map = Pool( processes = cpu_count() ).map
+	except NotImplementedError:
+		_map = map
+	return _map( _render, [ ( '{}_{:03}.{}'.format( basename, n, fmt ), fmt, size, graph ) for n, graph in enumerate( graphs ) ] )
 
 def gif( files, basename, delay = 100 ):
 	cmd = [ 'convert' ]
