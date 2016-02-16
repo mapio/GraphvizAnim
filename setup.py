0d48ef0b9bce93f0ea1af3ed2a1be3dd9536255b
Massimo Santini <santini@di.unimi.it>
2016-02-16 09:23:33 +0100

    Added PiPy stuff

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 from distutils.core import setup
 
+def read( path ):
+	with open( path, 'r' ) as f:
+		return f.read()
+
 setup(
 	name = 'GraphvizAnim',
-	version = '0.1-alpha',
+	version = '0.2-alpha',
 	description = 'A tool to create animated graph visualizations, based on graphviz',
+	long_description = read( 'README.md' ),
 	author = 'Massimo Santini',
 	author_email = 'santini@di.unimi.it',
 	url = 'https://github.com/mapio/GraphvizAnim',
+	license = 'GNU/GPLv3',
 	packages = [ 'gvanim' ],
+	keywords = 'drawing graph animation',
+	classifiers = [
+		'Development Status :: 3 - Alpha',
+		'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+		'Operating System :: Unix',
+		'Topic :: Software Development :: Libraries :: Python Modules'
+	]
 )
+
+
