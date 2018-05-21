30c577d7680aca5e652dbfd1cf71b7f34a8debc9
Massimo Santini <massimo.santini@gmail.com>
2018-05-21 10:02:24 +0200

    Preparing release 1.0.1

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ def read( path ):
 
 setup(
 	name = 'GraphvizAnim',
-	version = '0.3-alpha',
+	version = '1.0.1',
 	description = 'A tool to create animated graph visualizations, based on graphviz',
 	long_description = read( 'README.md' ),
+	long_description_content_type='text/markdown',
 	author = 'Massimo Santini',
 	author_email = 'santini@di.unimi.it',
 	url = 'https://github.com/mapio/GraphvizAnim',
@@ -16,7 +17,7 @@ setup(
 	packages = [ 'gvanim' ],
 	keywords = 'drawing graph animation',
 	classifiers = [
-		'Development Status :: 3 - Alpha',
+		'Development Status :: 5 - Production/Stable',
 		'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
 		'Operating System :: Unix',
 		'Topic :: Software Development :: Libraries :: Python Modules'
