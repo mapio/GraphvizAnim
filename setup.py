c78426360cb65ce12f7e633e52f1253c91e00da1
Massimo Santini <massimo.santini@gmail.com>
2019-02-17 15:57:28 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read( path ):
 
 setup(
 	name = 'GraphvizAnim',
-	version = '1.0.2',
+	version = '1.0.3',
 	description = 'A tool to create animated graph visualizations, based on graphviz',
 	long_description = read('README.md'),
 	long_description_content_type='text/markdown',
